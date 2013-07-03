d2a8265a43155724c796cc0dc2a622d13a3c37ae
lgoldstien <lgoldstien@gmail.com>
2013-07-03 09:31:09 +0200

    Add non-OCP logging class

---
+++ O/examples/non-ocp.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Log
+ * A class for logging output to a file
+ */
+
+class Log {
+	/**
+	 * @var string
+	 */
+	public $file;
+
+	function __construct() {
+
+	}
+
+	public function putLog( $app, $content, $level="log" ) {
+		$strLogEntry = "[" . date("D M j G:i:s T Y") . "] ";
+		$strLogEntry .= "{$app} - {$level} - {$content}";
+		return $strLogEntry;
+	}
+
+	public function logToFile( $line ) {
+		file_put_contents($this->file, $line . "\n", FILE_APPEND | LOCK_EX);
+	}
+}
+
+$log = new Log();
+$log->file = "/home/lgoldstien/logs/ocp.log";
+$line = $log->putLog( "OC1PTest", "This is a test log to see if the system works.", "info" );
+$log->logToFile( $line );
